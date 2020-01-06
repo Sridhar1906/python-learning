e1925dbebf302ead36b04ba9463cdff322e144ce
vivek1may <vivekananthan_ms@hotmail.com>
2020-01-06 22:17:08 +0530

    sort list question

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-learning
+
+### PYthon Learning with samples
+1. sort_list.py : sorting the list of numbers
---
+++ sort_list.py
@@ -0,0 +1,6 @@
+#Sort the following list 
+#Method 1 : with default function
+#Method 2 : with Loops
+
+
+data = [34,5,6,12,344,66,34,86,56]
